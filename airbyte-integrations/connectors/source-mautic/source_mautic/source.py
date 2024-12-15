f3f4928ca14aed19912d6f8e532a30d23a76d7db
Tomasz Wija <tomasz.wija@asanarebel.com>
2024-12-15 22:42:31 +0100

    asd

--- airbyte-integrations/connectors/source-mautic/source_mautic/source.py
+++ airbyte-integrations/connectors/source-mautic/source_mautic/source.py
@@ -1022,6 +1022,7 @@ class Contacts(IncrementalMauticStream):
         alt_cursor_value = latest_record.get(self.alt_cursor_field, "") or current_stream_state.get(self.alt_cursor_field)
         cursor_value = latest_record.get(self.cursor_field, "")
 
+        print(f"Current streamstate {current_stream_state}")
         print(f"Updating state with alt_cursor_field={alt_cursor_value}, cursor_field={cursor_value}")
         
         if current_stream_state:
