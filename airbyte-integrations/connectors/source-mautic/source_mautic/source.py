b4fed8d83177163abcd99357bcfd4b11d91b7107
Tomasz Wija <tomasz.wija@asanarebel.com>
2024-12-15 23:38:05 +0100

    qweqweqwe

--- airbyte-integrations/connectors/source-mautic/source_mautic/source.py
+++ airbyte-integrations/connectors/source-mautic/source_mautic/source.py
@@ -1040,6 +1040,7 @@ class Contacts(IncrementalMauticStream):
 
     def read_records(self, *args, **kwargs) -> Iterable[Mapping[str, Any]]:
         for record in super().read_records(*args, **kwargs):
+            print(f"streamstate {record}")
             alt_cursor_value = record.get(self.alt_cursor_field, "") or self.state.get(self.alt_cursor_field) or self.start_date
             cursor_value = record.get(self.cursor_field, "")
             updated_state = {
