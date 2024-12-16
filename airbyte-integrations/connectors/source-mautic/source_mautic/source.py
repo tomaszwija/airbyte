4bd8ac15c43224874439844b9bda593ae27054a1
Tomasz Wija <tomasz.wija@asanarebel.com>
2024-12-16 12:31:12 +0100

    qsasd

--- airbyte-integrations/connectors/source-mautic/source_mautic/source.py
+++ airbyte-integrations/connectors/source-mautic/source_mautic/source.py
@@ -274,6 +274,7 @@ class Contacts(IncrementalMauticStream):
 
     def read_records(self, *args, **kwargs) -> Iterable[Mapping[str, Any]]:
         for record in super().read_records(*args, **kwargs):
+            print(f"debugme: state {self.state} / record {record.get(self.alt_cursor_field, "")} / startdate {self.start_date}")
             current_alt_cursor_value = self.state.get(self.alt_cursor_field, self.start_date) or self.start_date
             current_cursor_value = self.state.get(self.cursor_field, "") or self.start_date
             alt_cursor_value = record.get(self.alt_cursor_field, "") or self.state.get(self.alt_cursor_field)
