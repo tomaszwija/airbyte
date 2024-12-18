34cd6610f9045f4e7dc738f9d89eae353b02c016
Tomasz Wija <tomasz.wija@asanarebel.com>
2024-12-18 11:08:21 +0100

    q'

--- airbyte-integrations/connectors/source-mautic/source_mautic/source.py
+++ airbyte-integrations/connectors/source-mautic/source_mautic/source.py
@@ -213,7 +213,7 @@ class EmailEvents(IncrementalMauticStream):
         super().__init__(**kwargs)
         self.url_base = url_base
         self.start_date = start_date
-        self.limit = 20000
+        self.limit = 10000
 
     def path(self, **kwargs) -> str:
 
